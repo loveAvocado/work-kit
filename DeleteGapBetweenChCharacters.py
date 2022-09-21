36d3cceeca3b19e277e618c68853f162ea07d10a
loveavocado <jeun.chien.t@Outlook.com>
2022-09-21 20:55:53 +0800

    從pdf複製摘要，去除空格

---
+++ DeleteGapBetweenChCharacters.py
@@ -0,0 +1,12 @@
+def mkStrWithNoGap(myStr):
+     newStr=""
+     for char in myStr:
+         if char not in " ":
+             newStr += char
+          
+     return newStr
+
+               
+
+             
+     
